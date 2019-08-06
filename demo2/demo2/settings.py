645c9f20b55bee01c3e9dd29353b1bee47a6e025
Martin <martinm.ordaz@gmail.com>
2019-08-06 10:16:15 -0700

    Update settings.py

--- demo2/demo2/settings.py
+++ demo2/demo2/settings.py
@@ -183,5 +183,5 @@ LOGGING = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'martinm.ordaz@gmail.com'
-EMAIL_HOST_PASSWORD = 'Moxw0w92'
+EMAIL_HOST_PASSWORD = '********'
 EMAIL_USE_TLS = True
